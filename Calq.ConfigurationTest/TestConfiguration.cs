fb7b4b1529cdbe55853f1b90a0a470237c30ced7
greg-chuchro <gchuchro@acm.org>
2021-08-07 06:40:10 +0900

    #1 load from json

---
+++ Calq.ConfigurationTest/TestConfiguration.cs
@@ -0,0 +1,16 @@
+﻿#pragma warning disable CS0649
+
+namespace Calq.ConfigurationTest {
+    class TestConfiguration {
+        public class Inner {
+            public int a;
+            public int b;
+        }
+
+        public int integer;
+        public bool boolean;
+        public Inner inner;
+        public string text;
+        public string nullText = "text";
+    }
+}
